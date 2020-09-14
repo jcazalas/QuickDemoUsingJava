0cff353f8795a447b835e78caa33e62779446b19
jcazalas <jcazalas@CSC004.fscwan.lcl>
2020-09-14 09:10:18 -0400

    Added Hello Github!

---
+++ QuickDemoUsingJava/src/quickdemousingjava/QuickDemoUsingJava.java
@@ -0,0 +1,15 @@
+// Name: Dr. Jonathan Cazalas
+// Date: 
+
+package quickdemousingjava;
+
+import java.util.*;
+import java.io.*;
+
+public class QuickDemoUsingJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Github!");
+    }
+
+}
